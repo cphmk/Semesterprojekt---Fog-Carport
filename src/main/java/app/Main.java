3fc7a47361b8f23f4aacfe50163e27ca1649edb2
FerdinandAV <ferdinandsjskole@gmail.com>
2024-06-06 15:18:52 +0200

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,9 +17,9 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 public class Main {
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgrespostgres";
+    private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "fogprojekt";
+    private static final String DB = "Fog";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
